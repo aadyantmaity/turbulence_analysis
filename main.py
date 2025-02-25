2681a97ec8fd06eab2a05eebbec253db1eac9492
Aadyant Maity <aadyantmaity@users.noreply.github.com>
2025-02-24 18:46:36 -0800

    santa ana plots updated

--- main.py
+++ main.py
@@ -32,18 +32,23 @@ year_chunks = list(range(min_year, max_year + 1, 2))
 year_chunks_general = list(range(min_year, max_year))
 year_chunks_3 = list(range(min_year, max_year + 1, 3))
 
-output_dir_svg = "svg_plots"
-detailed_general_dir_svg = os.path.join(output_dir_svg, "turbulence_detailed_general")
-detailed_burbank_dir_svg = os.path.join(output_dir_svg, "turbulence_detailed_burbank")
-detailed_offshore_dir_svg = os.path.join(output_dir_svg, "turbulence_detailed_offshore")
-general_3year_dir_svg = os.path.join(output_dir_svg, "general_3year")
-burbank_3year_dir_svg = os.path.join(output_dir_svg, "burbank_3year")
-offshore_3year_dir_svg = os.path.join(output_dir_svg, "offshore_3year")
-burbank_full_dir_svg = os.path.join(output_dir_svg, "burbank_full")
-general_full_dir_svg = os.path.join(output_dir_svg, "general_full")
-offshore_full_dir_svg = os.path.join(output_dir_svg, "offshore_full")
+# Directories for SVG plots
+output_dir_svg_general = "svg_plots/general"
+output_dir_svg_burbank = "svg_plots/burbank"
+output_dir_svg_santaana = "svg_plots/santaana"
+detailed_general_dir_svg = os.path.join(output_dir_svg_general, "turbulence_detailed_general")
+detailed_burbank_dir_svg = os.path.join(output_dir_svg_burbank, "turbulence_detailed_burbank")
+detailed_offshore_dir_svg = os.path.join(output_dir_svg_santaana, "turbulence_detailed_offshore")
+general_3year_dir_svg = os.path.join(output_dir_svg_general, "general_3year")
+burbank_3year_dir_svg = os.path.join(output_dir_svg_burbank, "burbank_3year")
+offshore_3year_dir_svg = os.path.join(output_dir_svg_santaana, "offshore_3year")
+burbank_full_dir_svg = os.path.join(output_dir_svg_burbank, "burbank_full")
+general_full_dir_svg = os.path.join(output_dir_svg_general, "general_full")
+offshore_full_dir_svg = os.path.join(output_dir_svg_santaana, "offshore_full")
 
-os.makedirs(output_dir_svg, exist_ok=True)
+os.makedirs(output_dir_svg_general, exist_ok=True)
+os.makedirs(output_dir_svg_burbank, exist_ok=True)
+os.makedirs(output_dir_svg_santaana, exist_ok=True)
 os.makedirs(detailed_general_dir_svg, exist_ok=True)
 os.makedirs(detailed_burbank_dir_svg, exist_ok=True)
 os.makedirs(detailed_offshore_dir_svg, exist_ok=True)
@@ -54,18 +59,23 @@ os.makedirs(general_full_dir_svg, exist_ok=True)
 os.makedirs(burbank_full_dir_svg, exist_ok=True)
 os.makedirs(offshore_full_dir_svg, exist_ok=True)
 
-output_dir_png= "png_plots"
-detailed_general_dir_png = os.path.join(output_dir_png, "turbulence_detailed_general")
-detailed_burbank_dir_png = os.path.join(output_dir_png, "turbulence_detailed_burbank")
-detailed_offshore_dir_png = os.path.join(output_dir_png, "turbulence_detailed_offshore")
-general_3year_dir_png = os.path.join(output_dir_png, "general_3year")
-burbank_3year_dir_png = os.path.join(output_dir_png, "burbank_3year")
-offshore_3year_dir_png = os.path.join(output_dir_png, "offshore_3year")
-burbank_full_dir_png = os.path.join(output_dir_png, "burbank_full")
-general_full_dir_png = os.path.join(output_dir_png, "general_full")
-offshore_full_dir_png = os.path.join(output_dir_png, "offshore_full")
+# Directories for PNG plots
+output_dir_png_general = "png_plots/general"
+output_dir_png_burbank = "png_plots/burbank"
+output_dir_png_santaana = "png_plots/santaana"
+detailed_general_dir_png = os.path.join(output_dir_png_general, "turbulence_detailed_general")
+detailed_burbank_dir_png = os.path.join(output_dir_png_burbank, "turbulence_detailed_burbank")
+detailed_offshore_dir_png = os.path.join(output_dir_png_santaana, "turbulence_detailed_offshore")
+general_3year_dir_png = os.path.join(output_dir_png_general, "general_3year")
+burbank_3year_dir_png = os.path.join(output_dir_png_burbank, "burbank_3year")
+offshore_3year_dir_png = os.path.join(output_dir_png_santaana, "offshore_3year")
+burbank_full_dir_png = os.path.join(output_dir_png_burbank, "burbank_full")
+general_full_dir_png = os.path.join(output_dir_png_general, "general_full")
+offshore_full_dir_png = os.path.join(output_dir_png_santaana, "offshore_full")
 
-os.makedirs(output_dir_png, exist_ok=True)
+os.makedirs(output_dir_png_general, exist_ok=True)
+os.makedirs(output_dir_png_burbank, exist_ok=True)
+os.makedirs(output_dir_png_santaana, exist_ok=True)
 os.makedirs(detailed_general_dir_png, exist_ok=True)
 os.makedirs(detailed_burbank_dir_png, exist_ok=True)
 os.makedirs(detailed_offshore_dir_png, exist_ok=True)
@@ -85,33 +95,33 @@ if __name__ == "__main__":
     #svg plots
 
     #PLOT TURBULENCE DETAILED
-    #plot_detailed_turbulence_general(df, "Detailed Turbulence Reports", detailed_general_dir_svg, True)
-    #plot_detailed_turbulence_burbank(burbank_turbulence, "Detailed Turbulence Reports - Burbank (BUR)", detailed_burbank_dir_svg, True)
+    plot_detailed_turbulence_general(df, "Detailed Turbulence Reports", detailed_general_dir_svg, True)
+    plot_detailed_turbulence_burbank(burbank_turbulence, "Detailed Turbulence Reports - Burbank (BUR)", detailed_burbank_dir_svg, True)
     plot_detailed_turbulence_general(offshore_flow, "Detailed Turbulence Reports - Offshore Flow", detailed_offshore_dir_svg, True)
 
     #PLOT TURBULENCE 3 YEAR CHUNKS
-    #plot_turbulence_by_3year_chunks(df, "Turbulence Reports", general_3year_dir_svg, False, True)
-    #plot_turbulence_by_3year_chunks(burbank_turbulence, "Turbulence Reports - Burbank (BUR)", burbank_3year_dir_svg, True, True)
+    plot_turbulence_by_3year_chunks(df, "Turbulence Reports", general_3year_dir_svg, False, True)
+    plot_turbulence_by_3year_chunks(burbank_turbulence, "Turbulence Reports - Burbank (BUR)", burbank_3year_dir_svg, True, True)
     plot_turbulence_by_3year_chunks(offshore_flow, "Turbulence Reports - Offshore Flow", offshore_3year_dir_svg, True, True)
     
     #PLOT TURBULENCE FULL Range
-    #plot_turbulence_full_range(df, "Turbulence Reports", general_full_dir_svg, False, True)
-    #plot_turbulence_full_range(burbank_turbulence, "Turbulence Reports - Burbank (BUR)", burbank_full_dir_svg, True, True)
+    plot_turbulence_full_range(df, "Turbulence Reports", general_full_dir_svg, False, True)
+    plot_turbulence_full_range(burbank_turbulence, "Turbulence Reports - Burbank (BUR)", burbank_full_dir_svg, True, True)
     plot_turbulence_full_range(offshore_flow, "Turbulence Reports - Offshore Flow", offshore_full_dir_svg, True, True)
 
     #png plots
 
     #PLOT TURBULENCE DETAILED
-    #plot_detailed_turbulence_general(df, "Detailed Turbulence Reports", detailed_general_dir_png, False)
-    #plot_detailed_turbulence_burbank(burbank_turbulence, "Detailed Turbulence Reports - Burbank (BUR)", detailed_burbank_dir_png, False)
+    plot_detailed_turbulence_general(df, "Detailed Turbulence Reports", detailed_general_dir_png, False)
+    plot_detailed_turbulence_burbank(burbank_turbulence, "Detailed Turbulence Reports - Burbank (BUR)", detailed_burbank_dir_png, False)
     plot_detailed_turbulence_general(offshore_flow, "Detailed Turbulence Reports - Offshore Flow", detailed_offshore_dir_png, False)
 
     #PLOT TURBULENCE 3 YEAR CHUNKS
-    #plot_turbulence_by_3year_chunks(df, "Turbulence Reports", general_3year_dir_png, False, False)
-    #plot_turbulence_by_3year_chunks(burbank_turbulence, "Turbulence Reports - Burbank (BUR)", burbank_3year_dir_png, True, False)
+    plot_turbulence_by_3year_chunks(df, "Turbulence Reports", general_3year_dir_png, False, False)
+    plot_turbulence_by_3year_chunks(burbank_turbulence, "Turbulence Reports - Burbank (BUR)", burbank_3year_dir_png, True, False)
     plot_turbulence_by_3year_chunks(offshore_flow, "Turbulence Reports - Offshore Flow", offshore_3year_dir_png, True, False)
 
     #PLOT TURBULENCE FULL Range
-    #plot_turbulence_full_range(df, "Turbulence Reports", general_full_dir_png, False, False)
-    #plot_turbulence_full_range(burbank_turbulence, "Turbulence Reports - Burbank (BUR)", burbank_full_dir_png, True, False)
+    plot_turbulence_full_range(df, "Turbulence Reports", general_full_dir_png, False, False)
+    plot_turbulence_full_range(burbank_turbulence, "Turbulence Reports - Burbank (BUR)", burbank_full_dir_png, True, False)
     plot_turbulence_full_range(offshore_flow, "Turbulence Reports - Offshore Flow", offshore_full_dir_png, True, False)
